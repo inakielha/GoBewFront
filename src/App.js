27a9ab82d4c59c7a3fad9d0b1eb29eeab96c9405
Galo Meggiolaro <88691710+GaloMegg@users.noreply.github.com>
2022-05-28 15:54:00 -0300

    Galo (#2)
    * solving order bugs
    
    * warning solved

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-import { GET_PRODUCTS } from './redux/actions';
-import { useEffect } from 'react';
 import Nav from './components/nav/Nav';
 import ProductCardContainer from './components/mainContent/ProductCardContainer';
 import CreationForm from './admin/components/products/CreationForm';
@@ -13,6 +10,7 @@ function App() {
     <div className="App">
       <Routes>
         <Route exact path='/' element={<><Nav />, <ProductCardContainer /></>} />
+
         <Route exact path='/admin/new' element={<CreationForm />} />
       </Routes>
     </div>
